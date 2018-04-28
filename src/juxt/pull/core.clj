4687b047401a1b8d66742d821954461bc30ec744
Luo Tianj <lt@basecity.com>
2018-04-29 00:23:02 +0800

    add minimum documentation for the new features

--- src/juxt/pull/core.clj
+++ src/juxt/pull/core.clj
@@ -3,8 +3,19 @@
    [juxt.pull.core.impl :as impl]))
 
 (defn pull
-  "Take a query, and some state. Return a map."
+  "Take a query, and some state. Return a map.
+  Optionally opts is map, which can have kv pairs:
+
+   - :shadow a shadow attributes map
+   - :stealth a stealth attributes set
+
+  the shadow attributes map defines attributes not really exists,
+  but can be calculated by a function takes a single argument (parent data),
+  returns a value as the attribute value.
+
+  all key inside stealth attributes set will not appeared in the
+  returned data as if non-exists, perfectly for sensitive data."
   ([data query]
    (pull data query nil))
-  ([data query shadow]
+  ([data query opts]
    (impl/pull data data query shadow)))
